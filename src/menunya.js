2b3811d4f333991d5ecb6681dc6edfbdc2b4977b
Hackers In Sri Lanka <113082337+hackersinsrilankaofc@users.noreply.github.com>
2022-10-08 16:42:28 +0530

    මගෙ සූටි මැනික...🌸

---
+++ src/menunya.js
@@ -0,0 +1,30 @@
+const menunya = `
+╭━━•›〘 ALL MENU 〙
+││
+││
+││ ➦❑  BUGMENU 
+││ ➦❑  FUNMENU 
+││ ➦❑  MAINMENU 
+││ ➦❑  VOICEMENU 
+││ ➦❑  GROUPMENU 
+││ ➦❑  OWNERMENU 
+││ ➦❑  ISLAMICMENU 
+││ ➦❑  SEARCHMENU 
+││ ➦❑  EPHOTOMENU 
+││ ➦❑  RANDOMMENU 
+││ ➦❑  TEKSPROMENU 
+││ ➦❑  PRIMBONMENU 
+││ ➦❑  CONVERTMENU 
+││ ➦❑  WEBZONEMENU
+││ ➦❑  DATABASEMENU 
+││ ➦❑  PHOTOOXYMENU
+││ ➦❑  DOWNLOADMENU
+││ ➦❑  ANONYMOUSMENU
+││
+│└─────────────
+└──────────────•⩵꙰ཱི࿐
+ ▬▭▬▭▬ ✦✧✦ ▬▭▬▭▬
+
+© ⏤͟͟͞𝑻𝑯𝑬 𝘽𝙐𝙂-𝘽𝙊𝙏-𝙈𝘿◔
+`
+exports.menunya = menunya
